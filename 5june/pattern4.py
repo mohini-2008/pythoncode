2f66001546715b0a28552804508d67b8590484a4
mohini-2008 <mohinidapkar88@gmail.com>
2026-06-08 21:34:14 +0530

    Create pattern4.py

---
+++ 5june/pattern4.py
@@ -0,0 +1,17 @@
+for i in range(3,0,-1):
+    for j in range(1,i+1):
+        if i==3:
+            print(" a",end='')
+        elif i==2:
+            print(" b",end='')
+        else:
+            print(" c",end='')
+    print()
+
+
+OUTPUT:
+
+ a a a
+ b b
+ c
+
