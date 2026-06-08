c3480ed8ec669546344c51224f2b1c35a6440bf4
mohini-2008 <mohinidapkar88@gmail.com>
2026-06-08 21:29:53 +0530

    Create pattern3.py

---
+++ 5june/pattern3.py
@@ -0,0 +1,18 @@
+for i in range(1,4):
+    for j in range(1,i+1):
+        if i==1:
+            print(" 1",end='')
+        elif i==2:
+            print(" 3",end='')
+        else:
+            print(" 5",end='')
+    print()
+
+
+OUTPUT:
+
+ 1
+ 3 3
+ 5 5 5
+
+
